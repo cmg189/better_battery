465dc993cf19b2d4cc74ed792ccbcf82f598568d
cmg189 <tx189@yahoo.com>
2022-10-22 02:21:59 -0500

    analogRead() to serial monitor

--- arduino.ino
+++ arduino.ino
@@ -1,12 +1,15 @@
 #define volt_pin A0    // pin recieving voltage from battery
 #define baud_rate 9600 // baud rate for serial monitor
 
+const int time_delay = 1500;
+
 void setup() {
   Serial.begin(baud_rate);
 
 }
 
 void loop() {
-  Serial.println(volt_pin);
+  Serial.println( analogRead(volt_pin) );
 
+  delay(time_delay);
 }
