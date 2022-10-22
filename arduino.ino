c653caab0017d380543a1204c3de2f13600e366c
cmg189 <tx189@yahoo.com>
2022-10-22 02:45:13 -0500

    improved accuracy of milivolts calcultion

--- arduino.ino
+++ arduino.ino
@@ -2,10 +2,11 @@
 #define baud_rate 9600 // baud rate for serial monitor
 
 const int time_delay = 1500;
-const float mv_per_count = 4.88;  // 5v / 1024 = 0.00488v
+const float mv_per_count = 4.54;  // 4.65v / 1024 = 0.00454v
 
 float num_counts = 0;
 float mili_volts = 0;
+
 void setup() {
   Serial.begin(baud_rate);
 
