3de4185af5bda2e9b620baf3e9366141ead75827
cmg189 <tx189@yahoo.com>
2022-10-22 02:39:11 -0500

    milivolts calculated

--- arduino.ino
+++ arduino.ino
@@ -2,14 +2,21 @@
 #define baud_rate 9600 // baud rate for serial monitor
 
 const int time_delay = 1500;
+const float mv_per_count = 4.88;  // 5v / 1024 = 0.00488v
 
+float num_counts = 0;
+float mili_volts = 0;
 void setup() {
   Serial.begin(baud_rate);
 
 }
 
 void loop() {
-  Serial.println( analogRead(volt_pin) );
+  num_counts = analogRead(volt_pin);
+
+  mili_volts = num_counts * mv_per_count;
+
+  Serial.println(mili_volts);
 
   delay(time_delay);
 }
